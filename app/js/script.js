6ce0bf185972c6fbcfb28ccd74561ae2e08522b7
post64bit <post64bit@gmail.com>
2022-02-10 16:30:42 +0300

    devswat

--- app/js/script.js
+++ app/js/script.js
@@ -17,8 +17,8 @@ $(document).ready(function () {
     let mouseCursor = document.querySelector('.cursor')
     window.addEventListener('mousemove', cursor)
     function cursor(e) {
-        mouseCursor.style.top = e.pageY + 'px'
-        mouseCursor.style.left = e.pageX + 'px'
+        mouseCursor.style.top = e.clientY + 'px'
+        mouseCursor.style.left = e.clientX + 'px'
     }
 
     let smallHeader = document.querySelector('.small-header')
